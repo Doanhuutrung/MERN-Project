54658cf184d82fb3938dfa766ffaefdcb5029773
Huu Trung <huutrungdoan22202@gmail.com>
2024-01-25 14:14:46 +0700

    fix error

--- src/app/api/[...nextauth]/route.js
+++ src/app/api/[...nextauth]/route.js
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github";
 
 const authOptions = {
-  provider: [
-    GitHubProvider({
+  providers: [
+    GithubProvider({
       clientId: "Iv1.fb58dfced60cde19",
       clientSecret: "94c01472fc8da79c0d1f7043108e4d900ed61ddc",
     }),
@@ -11,16 +11,18 @@ const authOptions = {
   callbacks: {
     async session({ session, token, user }) {
       session.user.username = session?.user?.name
-        .split("")
+        .split(" ")
         .join("")
         .toLocaleLowerCase();
+
       session.user.uid = token.sub;
+
       return session;
     },
   },
-  secret: "default_secreet_key",
+  secret: "default_secret_key",
 };
 
 const handler = NextAuth(authOptions);
 
-export {handler as GET , handler as POST }
+export { handler as GET, handler as POST };
--- src/app/browser/page.js
+++ src/app/browser/page.js
@@ -5,7 +5,10 @@ import { useSession } from "next-auth/react";
 
 export default function Browser() {
   const { data: session } = useSession();
+
   console.log(session, "session");
+
   if (session === null) return <UnauthPage />;
+  
   return <div> Browser</div>;
 }
