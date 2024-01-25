89599d8fead42c2a8c6e1a5c3430c46618711100
Huu Trung <huutrungdoan22202@gmail.com>
2024-01-25 15:17:11 +0700

    create manage account component for check account is logged

--- src/app/browser/page.js
+++ src/app/browser/page.js
@@ -1,14 +1,19 @@
 "use client";
 
+import ManageAccounts from "@/components/manage-accounts";
 import UnauthPage from "@/components/unauth-page";
+import { GlobalContext } from "@/context";
 import { useSession } from "next-auth/react";
+import { useContext } from "react";
 
 export default function Browser() {
+  const { loggedInAccount } = useContext(GlobalContext);
   const { data: session } = useSession();
 
   console.log(session, "session");
 
   if (session === null) return <UnauthPage />;
+  if (loggedInAccount === null) return <ManageAccounts />;
 
   return <div> Browser </div>;
 }
---
+++ src/components/manage-accounts/index.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ManageAccounts() {
+  return <div>ManageAccounts</div>;
+}
