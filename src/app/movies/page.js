7c66404d171c090da67d8f50bea66b1c147ce975
Huu Trung <huutrungdoan22202@gmail.com>
2024-01-25 15:30:51 +0700

    useContext for connect to loggedInAccount

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,11 +1,16 @@
 "use client";
 
+import ManageAccounts from "@/components/manage-accounts";
 import UnauthPage from "@/components/unauth-page";
+import { GlobalContext } from "@/context";
 import { useSession } from "next-auth/react";
+import { useContext } from "react";
 
 export default function Movies() {
+  const { loggedInAccount } = useContext(GlobalContext);
   const { data: session } = useSession();
   console.log(session);
-  if (session === null) return <UnauthPage/>;
+  if (session === null) return <UnauthPage />;
+  if (loggedInAccount === null) return <ManageAccounts />;
   return <div> Movies</div>;
 }
--- src/app/search/[query]/page.js
+++ src/app/search/[query]/page.js
@@ -1,11 +1,14 @@
 "use client";
 
 import UnauthPage from "@/components/unauth-page";
+import { GlobalContext } from "@/context";
 import { useSession } from "next-auth/react";
+import { useContext } from "react";
 
 export default function Search() {
   const { data: session } = useSession();
+  const { loggedInAccount } = useContext(GlobalContext);
   console.log(session);
   if (session === null) return <UnauthPage />;
-  return <div> Browser</div>;
+  return <div> Search </div>;
 }
