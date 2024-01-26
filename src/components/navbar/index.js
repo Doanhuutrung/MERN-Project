3f36a5387af413914bcc16eaae4b35cae1ab57d8
Huu Trung <huutrungdoan22202@gmail.com>
2024-01-26 13:50:27 +0700

    add link and logo netflix for navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,9 +1,63 @@
-"use client"
+"use client";
 
-export default function Navbar () {
-  return <div className="relative">
-    <header>
-      
-    </header>
-  </div>
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Navbar() {
+  const { data: session } = useSession();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const router = useRouter();
+  const pathName = usePathname();
+  const menuItems = [
+    {
+      id: "home",
+      title: "home",
+      path: "/browser",
+    },
+    {
+      id: "tV",
+      title: "TV",
+      path: "/tv",
+    },
+    {
+      id: "movies",
+      title: "Movies",
+      path: "/movies",
+    },
+    {
+      id: "my-list",
+      title: "My List",
+      path: "/mylist",
+    },
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) setIsScrolled(true);
+      else setIsScrolled(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className="relative">
+      <header
+        className={`header ${isScrolled && "bg-[#141414]"} hover:bg-[1414141]}`}
+      >
+        <div className="flex items-center space-x-2 md:space-x-10">
+          <img
+            src="https://rb.gy/ulxxee"
+            width={120}
+            height={120}
+            alt="Netflix-logo"
+            className="cursor-pointer object-contain"
+            onClick={() => router.push("/browser")}
+          />
+        </div>
+      </header>
+    </div>
+  );
 }
