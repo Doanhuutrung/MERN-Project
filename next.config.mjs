966613f7a9d7a44d821417d87ea87456aab6c8d9
Huu Trung <huutrungdoan22202@gmail.com>
2024-01-30 10:51:50 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "http://localhost:3000/",
+        port: "",
+        pathname: "https://rb.gy/ulxxee",
+      },
+    ],
   },
 };
 
