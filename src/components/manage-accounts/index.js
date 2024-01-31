07ce7113821937313ac5df4f111c773f1abacb77
Huu Trung <huutrungdoan22202@gmail.com>
2024-01-31 23:15:18 +0700

    update button add account

--- src/components/manage-accounts/index.js
+++ src/components/manage-accounts/index.js
@@ -7,6 +7,7 @@ import CircleLoader from "../circle-loader";
 import AccountForm from "./account-form";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { UserPlusIcon } from "@heroicons/react/24/outline";
 import PinContainer from "./pin-container";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -161,10 +162,8 @@ export default function ManageAccounts() {
                 <li
                   className="max-w-[200px] w-[155px] cursor-pointer flex flex-col items-center gap-3 min-w-[200px]"
                   key={item._id}
-                  onClick={
-                    showDeleteIcon
-                      ? null
-                      : () => setShowPinContainer({ show: true, account: item })
+                  onClick={() =>
+                    setShowPinContainer({ show: true, account: item })
                   }
                 >
                   <div className="relative">
@@ -198,7 +197,7 @@ export default function ManageAccounts() {
                     ) : null}
                   </div>
                   <span className="mb-4">{item.name}</span>
-                  <svg
+                  {/* <svg
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
@@ -214,7 +213,7 @@ export default function ManageAccounts() {
                       d="M7 6C7 3.23858 9.23858 1 12 1C14.7614 1 17 3.23858 17 6V7H19C20.1046 7 21 7.89543 21 9V18.6529C21 19.6274 20.2885 20.4855 19.2814 20.6076C18.0287 20.7593 15.492 21 12 21C8.50801 21 5.97128 20.7593 4.71855 20.6076C3.71153 20.4855 3 19.6274 3 18.6529V9C3 7.89543 3.89543 7 5 7H7V6ZM15 6V7H9V6C9 4.34315 10.3431 3 12 3C13.6569 3 15 4.34315 15 6ZM5 9V18.627C6.19927 18.7708 8.63769 19 12 19C15.3623 19 17.8007 18.7708 19 18.627V9H5ZM11 12V16H13V12H11Z"
                       fill="currentColor"
                     ></path>
-                  </svg>
+                  </svg> */}
                 </li>
               ))
             : null}
@@ -222,9 +221,9 @@ export default function ManageAccounts() {
           {accounts && accounts.length < 4 ? (
             <li
               onClick={() => setShowAccountForm(!showAccountForm)}
-              className="border text-black bg-[#e5b109] font-bold text-lg border-black max-w-[200px] rounded min-w-[84px] max-h-[200px] min-h-[84px] w-[155px] h-[155px] cursor-pointer flex justify-center items-center"
+              className="border text-black bg-[#e5b109] font-bold text-lg border-black max-w-[200px] rounded min-w-[84px] max-h-[200px] min-h-[84px] w-[155px] h-[155px] cursor-pointer flex justify-center items-center ml-5"
             >
-              Add Account
+              <UserPlusIcon width={80} height={80}/>
             </li>
           ) : null}
         </ul>
